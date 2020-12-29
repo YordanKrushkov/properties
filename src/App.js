f83051031985c02a850d6061417e6bfda5f88b90
Yordan Krushkov <yordankrushkov@gmail.com>
2020-12-29 15:34:39 +0000

    convert to hooks

--- src/App.js
+++ src/App.js
@@ -8,40 +8,39 @@ import Login from './components/login'
 import Register from './components/Register'
 import Rent from './components/Rent'
 import Sell from './components/Sell';
-import { Component } from 'react';
+import {useContext } from 'react';
 import UserContext from './Context';
 
-
-class App extends Component {
-static contextType=UserContext;
-render() {
-  let PostPage;
-  const {loggedIn}=this.context;
-  if(loggedIn){
-    PostPage=<Route path='/post' component={PostForm}/>
+const App=(props)=>{
+  const context=useContext(UserContext);
+ 
+    let PostPage;
+    const {loggedIn}=context;
+    if(loggedIn){
+      PostPage=<Route path='/post' component={PostForm}/>
+    }
+    else{
+      PostPage=null;
+    }
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Header props={props} />
+          <Switch>
+          <Route exact path='/' component={Body}/>
+          {PostPage}
+          <Route path='/login' component={Login}/>
+          <Route path='/signout' component={Body}/>
+          <Route path='/register' component={Register}/>
+          <Route path='/rent' component={Rent}/>
+          <Route path='/sell' component={Sell}/>
+          </Switch>
+  
+  
+        </div>
+      </BrowserRouter>
+    )
+  
   }
-  else{
-    PostPage=null;
-  }
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header props={this.props} />
-        <Switch>
-        <Route exact path='/' component={Body}/>
-        {PostPage}
-        <Route path='/login' component={Login}/>
-        <Route path='/signout' component={Body}/>
-        <Route path='/register' component={Register}/>
-        <Route path='/rent' component={Rent}/>
-        <Route path='/sell' component={Sell}/>
-        </Switch>
-
-
-      </div>
-    </BrowserRouter>
-  )
-} 
-}
 
 export default App;
--- src/components/Rent/index.js
+++ src/components/Rent/index.js
@@ -1,48 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './index.module.css'
 import CardElement from "../cards/card-elemtent";
-import UserContext from '../../Context';
 
-class Rent extends Component {
-   
-    state = {
-        properties: []
-    }
-    static contextType=UserContext;
-    getData = async () => {
+
+const Rent = () => {
+    const [properties, getProperties] = useState([]);
+    const getData = async () => {
         const promise = await fetch('https://properties-3e020-default-rtdb.firebaseio.com/properties.json');
         const properties = await promise.json();
-        this.setState({
-            properties
-        })
+        getProperties(
+            properties)
 
     }
-
-    renderProperties() {
-        let { properties } = this.state
+    const renderProperties = () => {
         return Object.keys(properties).map((propertie) => {
             if (properties[propertie].hasOwnProperty('rent')) {
                 let item = properties[propertie]
                 let key = propertie;
 
-                return  CardElement(item, key)
-                
+                return CardElement(item, key)
             }
         })
     }
+    useEffect(() => {
+        getData();
+    }, [])
 
-    componentDidMount() {
-        this.getData();
-    }
+    return (
+        <div className={ styles.wrapper } >
+            {renderProperties() }
+        </div>
 
-    render() {
-        return (
-            <div className={styles.wrapper} >
-                {this.renderProperties() }
-            </div>
-
-        )
-    }
+    )
 }
 
 export default Rent
