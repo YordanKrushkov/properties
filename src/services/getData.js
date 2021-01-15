ea45588121b700f86994767c5a00bbdc220f9202
Yordan Krushkov <yordankrushkov@gmail.com>
2021-01-15 11:21:12 +0000

    add new login

--- src/services/getData.js
+++ src/services/getData.js
@@ -2,7 +2,7 @@ import CardElement from "../components/cards/card-elemtent";
 
 export default {
     async getData(getProperties) {
-        const promise = await fetch('https://properties-3e020-default-rtdb.firebaseio.com/properties.json');
+        const promise = await fetch('https://properties-3e020-default-rtdb..com/properties.json');
         const properties = await promise.json();
         getProperties(properties)
     },
